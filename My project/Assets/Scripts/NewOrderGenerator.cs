0dadb754e2fe99c68869769cbfcf53310dd9bcd4
gfyfaye <gogograce999@gmail.com>
2023-04-23 11:31:48 -0700

    generate new order attempt
    does not work yet but i push just in case

---
+++ My project/Assets/Scripts/NewOrderGenerator.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class NewOrderGenerator : MonoBehaviour
+{
+
+
+    public TextMeshProUGUI orderText;
+
+    //presets of variables
+
+    bool red_sauce = false;
+    bool cheese = false;
+    bool pepperoni = false;
+    bool mushroom = false;
+    bool olive = false;
+    string order = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Click button to view new order!");
+    }
+
+    //called everytime button is pressed
+    public void ButtonPressed()
+    {
+        GenerateNewOrder();
+        orderText.GetComponent<TextMeshProUGUI>().text = "Please make me a pizza with: \n" + order;
+    }
+
+    // randomly generating a new order
+
+    public void GenerateNewOrder()
+    {
+        BooleanGenerator boolGen = new BooleanGenerator();
+
+        red_sauce = boolGen.NextBoolean();
+        cheese = boolGen.NextBoolean();
+        pepperoni = boolGen.NextBoolean();
+        mushroom = boolGen.NextBoolean();
+        olive = boolGen.NextBoolean();
+
+        printNewOrder(red_sauce, cheese, pepperoni, mushroom, olive);
+    }
+
+    public void printNewOrder(bool r_s, bool ch, bool p, bool m, bool o)
+    {
+        if (r_s) { order = order + "\nRed Sauce"; }
+        else { order = order + "\nGreen Sauce"; }
+        if (ch) { order = order + "\nCheese"; }
+        if (p) { order = order + "\nPepperoni"; }
+        if (m) { order = order + "\nMushroom"; }
+        if (o) { order = order + "\nOlive"; }
+
+        //print out order 
+    }
+
+    public class BooleanGenerator
+    {
+        System.Random rnd;
+
+        public BooleanGenerator()
+        {
+            rnd = new System.Random();
+        }
+
+        public bool NextBoolean()
+        {
+            return rnd.Next(0, 2) == 1;
+        }
+    }
+
+}
