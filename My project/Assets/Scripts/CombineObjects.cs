5e033376f2c6158a7bc65818f2a38c34c983ea99
Omg577 <66808489+Omg577@users.noreply.github.com>
2023-04-26 08:18:10 -0700

    Can now combine game objects - woooooo

---
+++ My project/Assets/Scripts/CombineObjects.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+public class CombineObjects : MonoBehaviour
+{
+    public Transform parentObject;
+
+    private GameObject draggingObject;
+    private Vector3 offset;
+
+    private void OnMouseDown()
+    {
+        // Check if the mouse is over a game object
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if (hit.collider != null)
+        {
+            // Set the dragging object and its offset from the mouse position
+            draggingObject = hit.collider.gameObject;
+            offset = draggingObject.transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        // Check if there is a dragging object
+        if (draggingObject != null)
+        {
+            // Move the dragging object to the mouse position with the offset
+            draggingObject.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        // Check if there is a dragging object and it is over the parent object
+        if (draggingObject != null && parentObject != null && parentObject.GetComponent<Collider2D>().bounds.Contains(draggingObject.transform.position))
+        {
+            // Combine the dragging object with the parent object
+            draggingObject.transform.parent = parentObject;
+            draggingObject.GetComponent<SpriteRenderer>().sortingOrder = 1;
+        }
+
+        // Reset the dragging object
+        draggingObject = null;
+    }
+}
