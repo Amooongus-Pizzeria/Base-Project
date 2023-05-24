575c1cbfc543683070232704d52836cee6d70045
gfyfaye <gogograce999@gmail.com>
2023-05-24 10:29:43 -0700

    attempt garbage slicing

--- My project/Assets/Scripts/SliceMouseController.cs
+++ My project/Assets/Scripts/SliceMouseController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class SliceMouseController : MonoBehaviour
 {
+
+    [SerializeField] private GameObject slicePrefab;
+    private Vector2 startPoint;
+    private LineRenderer lineRenderer;
+    private bool isDrawing = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 2;
+        lineRenderer.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            lineRenderer.SetPosition(0, startPoint);
+            lineRenderer.enabled = true;
+            isDrawing = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isDrawing = false;
+            lineRenderer.enabled = false;
+        }
+
+        if (isDrawing)
+        {
+            Vector2 currentPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            lineRenderer.SetPosition(1, currentPoint);
+        }
     }
 }
--- My project/Assets/Scripts/garbagecancontroller.cs
+++ My project/Assets/Scripts/garbagecancontroller.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class garbagecancontroller : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+
     // Start is called before the first frame update
-    void Start()
+    /*void Start()
     {
-        
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (interactObject != null && interactObject.GetComponent<Collider2D>().bounds.Intersects(spriteRenderer.bounds))
+        {
+            Destroy(interactObject);
+
+        }
+    }*/
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
     }
 }
